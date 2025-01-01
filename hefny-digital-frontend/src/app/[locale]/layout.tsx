a789db7f4ccb6df088e9f78a91159381c288fc97
Mohamed Hefny <mohhef@amazon.com>
2025-01-01 08:06:25 -0800

    Fix

--- hefny-digital-frontend/src/app/[locale]/layout.tsx
+++ hefny-digital-frontend/src/app/[locale]/layout.tsx
@@ -10,22 +10,24 @@ export function generateStaticParams() {
   return [{ locale: "en" }, { locale: "ar" }];
 }
 
-export default async function LocaleLayout({
-  children,
-  params,
-}: {
+type LayoutProps = {
   children: ReactNode;
   params: { locale: string };
-}) {
+};
+
+export default async function LocaleLayout({
+  children,
+  params: { locale },
+}: LayoutProps) {
   let messages;
-  const { locale } = await params;
-  console.log(locale);
+
   try {
     messages = (await import(`../../../messages/${locale}.json`)).default;
   } catch (error) {
-    console.log(error);
+    console.error(error);
     notFound();
   }
+
   return (
     <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
       <body className={inter.className}>
