d5a41d54c9439e5ff6f1739708762ff7a5f17fb0
Mohamed Hefny <mohhef@amazon.com>
2025-01-01 08:21:50 -0800

    Fix

--- hefny-digital-frontend/src/app/[locale]/page.tsx
+++ hefny-digital-frontend/src/app/[locale]/page.tsx
@@ -6,8 +6,8 @@ import About from "../../components/About";
 import Contact from "../../components/Contact";
 import Footer from "../../components/Footer";
 
-export default async function Home({ params }: { params: { locale: string } }) {
-  const { locale } = await params;
+export default function Home({ params }: { params: { locale: string } }) {
+  const { locale } = params; // No need to use `await` here
   return (
     <div className="min-h-screen bg-gray-50">
       <Navbar locale={locale} />
