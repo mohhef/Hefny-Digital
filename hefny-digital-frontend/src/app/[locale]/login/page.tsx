006efe651bf3a470bf218353f4357e776b10b059
Mohamed Hefny <mohhef@amazon.com>
2025-01-01 18:47:30 -0800

    Fix

--- hefny-digital-frontend/src/app/[locale]/login/page.tsx
+++ hefny-digital-frontend/src/app/[locale]/login/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter } from "@/i18n/routing";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
